d53e9d717f9b99ca7024052e6628acd1983b258c
Eisha Fatima Shah <140654347+k200383@users.noreply.github.com>
2025-09-20 23:12:41 +0500

    Add files via upload

---
+++ src/store.ts
@@ -0,0 +1,10 @@
+///store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducer/rootReducer';
+
+const store = configureStore({
+    reducer: rootReducer
+});
+
+export type AppDispatch = typeof store.dispatch;
+export default store;
